d8f7adf2224868d217e151eb16b94178c31b2baa
Nazar Chuiko <rondelman15@gmail.com>
2021-10-08 20:16:10 +0300

    Task_2

--- cgi-bin/form.py
+++ cgi-bin/form.py
@@ -1,4 +1,18 @@
 import cgi
+import os
+import http.cookies
+
+
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+count = cookie.get("count")
+if count is None:
+    print("Set-cookie: count=1")
+else:
+    c = int(count.value) + 1
+    print(f"Set-cookie: count={c}")
+
+
+
 
 form = cgi.FieldStorage()
 text1 = form.getfirst("TEXT_1", "не задано")
@@ -21,5 +35,11 @@ print("<p>User text: {}</p>".format(text1))
 print("<p>User text: {}</p>".format(text2))
 print("<p>Lessons: {}".format(', '.join(checkbox)))
 print("<p>language: {}".format(', '.join(language)))
+print(cookie.get("count"))
+print("<hr>")
+for k, v in os.environ.items():
+    print(f"<p>Key = {k} value = {v}</p>")
+
 print("""</body>
         </html>""")
+
